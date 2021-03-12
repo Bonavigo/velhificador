73967882b029f5b129c9ab655b690dad1bcbe5cb
Bruno Bonavigo <71879062+Bonavigo@users.noreply.github.com>
2021-03-12 17:27:16 -0300

    Atualização no app.js e no index.html
    Mudança no funcionamento dos parâmetros que geram o velho e adição de um alerta do Bootstrap com os parâmetros usados no texto.

--- js/app.js
+++ js/app.js
@@ -1,15 +1,25 @@
 function gera_caracteristicas() {
-	const veio_berrao = Math.random() >= 0.5;
+	let veio_berrao = Math.random() >= 0.5;
+	let veio_fininho = null;
 	if (veio_berrao === false) {
-		const veio_fininho = Math.random() >= 0.5;
+		veio_fininho = true;
 	} else {
-		const veio_fininho = false;
+		veio_fininho = false;
 	}
-	const veio_fininho = Math.random() >= 0.5;
-	const veio_dos_emoji = Math.random() >= 0.5;
-	const veio_dos_ponto = true;
+	let veio_dos_emoji = true;
+	let veio_dos_ponto = true;
 	var veio = {veio_fininho:veio_fininho, veio_berrao:veio_berrao, veio_dos_emoji:veio_dos_emoji, veio_dos_ponto:veio_dos_ponto};
 	return veio;
+}
+function retornar_parametros(objeto) {
+	var gritao = caracteristicas.veio_berrao === true ? 'Sim' : 'Não';
+	var fala_fino = caracteristicas.veio_fininho === true ? 'Sim' : 'Não';
+	var pontuador = caracteristicas.veio_dos_ponto === true ? 'Sim' : 'Não';
+	var emojeiro = caracteristicas.veio_dos_emoji === true ? 'Sim' : 'Não';
+	const texto = "<strong>Parâmetros:</strong><br>Velho berrão: "+gritao+"<br>Velho fala fino: "+fala_fino+"<br>Velho que usa pontos: "+pontuador+"<br>Velho dos Emojis: "+emojeiro;
+	const alerta_parametros = document.getElementById("parametros");
+	alerta_parametros.style.display = 'block';
+	document.getElementById("txt_parametros").innerHTML = texto;
 }
 document.addEventListener('DOMContentLoaded', function() {
 	var velhos = ["pelo Seu Zé", "pelo Manoel", "pelo Edivaldo", "pelo Geraldo", "pelo Evaristo", "pelo Arnaldo Messias", "pela Tereza", "pelo João Costa", "pela Dircea", "pelo Ezequiel", "pelo Emanoel", "pelo Edivaldo", "pela Alzira", "pela Aparecida", "pela Fátima", "pela Adelaide", "pelo Casemiro", "pelo Jair", "pelo Aristênio", "pelo Balbino", "pelo Baltazar", "pelo Astolfo", "pelo Rodolfo", "pelo Adolfo", "pela Cleide", "pela Marlene", "pela Veranice", "pelo Cleber", "pela Mariângela", "pela Josefa", "pelo Raimundo", "pela Terezinha", "pelo Márcio"];
@@ -45,18 +55,17 @@ function veio_fala_fino(string) {
 function velhificar() {
 	caracteristicas = gera_caracteristicas();
 	var string = document.getElementById("texto").value;
-	if (caracteristicas.veio_dos_emoji === true) {
-		string = veio_dos_emoji(string);
-	}
+	string = veio_dos_emoji(string);
 	if (caracteristicas.veio_dos_ponto === true) {
 		string = veio_dos_ponto(string);
 	}
-	if (caracteristicas.veio_fininho === true) {
+	if (caracteristicas.veio_fininho === true && caracteristicas.veio_berrao === false) {
 		string = veio_fala_fino(string);
-	}
-	if (caracteristicas.veio_berrao === true) {
+	} 
+	if (caracteristicas.veio_berrao === true && caracteristicas.veio_fininho === false) {
 		string = veio_gritao(string);
 	}
 	document.getElementById("texto").value = string;
+	retornar_parametros(caracteristicas);
 	return string;
 }
